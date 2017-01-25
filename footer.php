7cf39739ab169fd7c8c7799f08f7ce69c89f0c5b
Denton John <kurgaev101@gmail.com>
2017-01-25 15:50:35 +0500

    day3

---
+++ footer.php
@@ -0,0 +1,27 @@
+<div class="wrapper">
+    <header class="container-fluid">
+		<div class="row top">
+		
+			<div class="logo col-xs-6 col-sm-6 col-md-4 col-lg-4">
+				<img class="logo1" src="images/logo.png" alt="">
+				<img class="logo2" src="images/mini-logo.png" alt="">
+			</div>
+			
+			<div class="text hidden-xs hidden-sm col-md-5 col-lg-5">
+				<div class="line1">Дистрибьютор Donaldson в Санкт-Петербурге</div>
+				<div class="line2">Поставка оборудования и сервисное обслуживание</div>
+			</div>
+			
+			<div class="contact col-xs-6 col-sm-6 col-md-3 col-lg-3">
+				<div class="city">г. Санкт-Петербург</div>
+				<div class="phone">
+					<span class="phone-code">+7 (812) </span>
+					<span class="phone-number">646-73-83</span>
+				</div>
+				<div class="button">
+					<a href="#">Оформить заявку</a>
+				</div>
+			</div>
+        </div>
+    </header>
+</div>
--- index.php
+++ index.php
@@ -1,10 +1,64 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- 	<?php include ("header.php"); ?> 
- </body>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> 
+    <link rel="stylesheet" href="css/header_footer.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>TITLE</title>
+</head>
+<body>
+    <?php include ("header.php"); ?>
+    
+    <main class="container-fluid">
+        <div class="row">
+            <div class="slider col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <img class="img-responsive" src="images/slide1.jpg" alt="">
+                <img class="img-responsive" src="images/slide2.jpg" alt="">
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h1>Продукция Donaldson</h1>
+                <p>Во многих промышленных сферах, эффективная фильтрация - решающий фактор для успешной защиты здоровья, окружающей среды и технологических процессов. В качестве партнера для промышленности, мы проводим исследования, разрабатываем и поставляем инновационные системы фильтрации для всех типов пыли, дыма и тумана, помогая улучшить качество воздуха и повысить эффективность производства.</p>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="grid col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <img src="images/PowerCore_dust_collectors_208x208_EMEA.jpg" alt="">
+                <a href="#">Пылеулавливатели PowerCore</a>
+            </div>
+            <div class="grid col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <img src="images/Downflo_dust_collectors_208x208_EMEA.jpg" alt="">
+                <a href="#">Картриджные пылеулавливатели</a>
+            </div>
+            <div class="grid col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <img src="images/Filters_for_donaldson_units_208x208_EMEA.jpg" alt="">
+                <a href="#">Сервис и комплектующие</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="grid col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <img src="images/WSO_dust_collectors_208x208_EMEA.JPG" alt="">
+                <a href="#">Улавливатели маслянного тумана</a>                
+            </div>
+            <div class="grid col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                <img src="images/Dalamatic_dust_collectors_208x208_EMEA.jpg" alt="">
+                <a href="#">Рукавные пылеулавители</a>       
+            </div>
+        </div>
+        <div class="row">
+            <div class="about col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h1>О компании</h1>
+                <p>Компания Donaldson (Donaldson Filtration Solutions) была образована в 1915 году в США. Основателем компании является Фрэнк Дональдсон - человек, который разработал первый фильтр для двигателей внутреннего сгорания. В 1915 году штат компании состоял всего из трех человек.</p>
+                <p>Сегодня Donaldson Company Inc. является мировым лидером в производстве фильтров, глушителей и их компонентов для автомобилей (грузовики, автобусы), сельскохозяйственной техники (комбайны, трактора и др.), строительной и дорожно-строительной техники (автокраны, грейдеры, асфальтоукладчики, катки и др.), спецтехники (погрузчики, карьерная и портовая техника и др.). Кроме этого компания разрабатывает и выпускает комплексные решения для промышленных систем фильтрации.</p>
+                <p>В настоящее время компания Donaldson - это более 30 заводов и представительств в 16 странах мира, штат сотрудников, превышающий 12500 человек, из которых свыше 400 - инженеры. Благодаря большому вниманию, которое компания Donaldson уделяет новым разработкам, начиная с 1915 года и по сегодняшний день, все основные изобретения и патенты в области систем фильтрации и акустических технологий принадлежат компании Donaldson - свыше 400 патентов зарегистрировано патентным бюро США.</p>
+                <p>Donaldson Company Inc. является членом S&P, MidCap 400 и Russell 1000. Акции котируются на Нью-Йоркской фондовой бирже под символом DCI.</p>
+            </div>
+        </div>
+    </main>
+    
+    <?php include ("footer.php"); ?>
+</body>
 </html>
-
-<!-- include ("header.html");  -->
